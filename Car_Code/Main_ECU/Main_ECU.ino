c834be7232baf723f1ea33c9eec749b2486e2590
karvindass <karvin9@gmail.com>
2016-10-13 20:16:55 -0400

    updated base code

--- Car_Code/Main_ECU/Main_ECU.ino
+++ Car_Code/Main_ECU/Main_ECU.ino
@@ -4,5 +4,14 @@ FlexCAN CAN(500000);
 static CAN_message_t msg;
 
 // setup code
+void setup() {
+    Serial.begin(115200); // init serial for PC communication
+
+    CAN.begin(); // init CAN system
+    Serial.println("CAN system and serial communication initialized");
+}
 
 // loop code
+void loop() {
+  
+}
