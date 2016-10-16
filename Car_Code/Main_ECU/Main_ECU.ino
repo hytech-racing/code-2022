647e9719aabb7626464c77cd53419d880868e295
vchen36 <vchen36@gatech.edu>
2016-10-16 17:26:22 -0400

    Added framework for states for startup shutdown

--- Car_Code/Main_ECU/Main_ECU.ino
+++ Car_Code/Main_ECU/Main_ECU.ino
@@ -2,7 +2,14 @@
 
 FlexCAN CAN(500000);
 static CAN_message_t msg;
+int OKHS = 0;
+int DISCHARGE_OK = 0;
+boolean startPressed = false;
+int timer = 0;
+boolean startupDone = false;
 
+enum state { GLVinit=0, waitIMD, waitBMS, waitStartButton, closeLatch, openLatch, AIROpen, AIRClose, waitInverter, readySoundOn, drive };
+state = GLVinit;
 // setup code
 void setup() {
     Serial.begin(115200); // init serial for PC communication
@@ -13,5 +20,22 @@ void setup() {
 
 // loop code
 void loop() {
-  
+    if (!startupDone) {
+        switch (state) {
+            case GLVinit: 
+            case waitIMD:
+            case waitBMS:
+            case waitStartButton:
+            case closeLatch:
+            case openLatch:
+            case AIROpen:
+            case AIRClose:
+            case waitInverter:
+            case readySoundOn:
+            case drive:
+            
+        }
+    } else {
+        
+    }
 }
