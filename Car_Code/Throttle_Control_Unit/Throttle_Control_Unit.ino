6333835e07831e02bbc3f01f78d5cfa4e3025b3b
Brian Cai <briancai@gatech.edu>
2016-10-18 20:17:36 -0400

    yolo

--- Car_Code/Throttle_Control_Unit/Throttle_Control_Unit.ino
+++ Car_Code/Throttle_Control_Unit/Throttle_Control_Unit.ino
@@ -15,41 +15,40 @@ int voltageThrottlePedal2 = 0; //voltage of 2nd throttle
 const int BRAKE_ANALOG_PORT = 3; //analog port of brake sensor
 const int THROTTLE_PORT_1 = 6; //first throttle sensor port
 const int THROTTLE_PORT_2 = 9; //second throttle sensor port
+const int minPedal = 0;//compare pedal travel
+const int maxPedal = 0;//compare pedal travel
 
 //FSAE requires that torque be shut off if an implausibility persists for over 100 msec (EV2.3.5).
 //A deviation of more than 10% pedal travel between the two throttle sensors
 //A failure of position sensor wiring which can cause an open circuit, short to ground, or short to sensor power.
 bool torqueShutdown = false; //
 
-//To detect an open circuit
-//enable the pullup resistor on the Teensy input pin >>>
-//open circuit will show a high signal outside of the working range of the sensor. 
-
-
-//To detect a position sensor wiring failure
-//find the ranges of values coming from each sensor during normal operation of the foot pedals
-//Any values outside of these ranges could be caused by an open circuit, short to ground, or short to sensor power.
-
-//Error Message Instructions
-//an error message should be sent out on CAN Bus detailing which implausibility has been detected. 
-//periodically sent until the implausibility ceases to exist. 
-//If the implausibility ceases, a corresponding message should be sent on CAN Bus. 
-//If an implausibility ceases to be detected, normal throttle controls should be reinstated
-//i.e. the vehicle does not need to be restarted to reset an implausibility fault.
-
-
-
-// timer
-
-
 // setup code
-
 void setup() {
     Serial.begin(115200); // init serial for PC communication
 
     CAN.begin(); // init CAN system
     Serial.println("CAN system and serial communication initialized");
+    //To detect an open circuit
+    //enable the pullup resistor on the Teensy input pin >>>
+    pinMode(BRAKE_ANALOG_PORT, INPUT_PULLUP);
+    pinMode(THROTTLE_PORT_1, INPUT_PULLUP);
+    pinMode(THROTTLE_PORT_2, INPUT_PULLUP);
+    //open circuit will show a high signal outside of the working range of the sensor. 
+        
+    
+    //To detect a position sensor wiring failure
+    //find the ranges of values coming from each sensor during normal operation of the foot pedals
+    //Any values outside of these ranges could be caused by an open circuit, short to ground, or short to sensor power.
+    
+    //Error Message Instructions
+    //an error message should be sent out on CAN Bus detailing which implausibility has been detected. 
+    //periodically sent until the implausibility ceases to exist. 
+    //If the implausibility ceases, a corresponding message should be sent on CAN Bus. 
+    //If an implausibility ceases to be detected, normal throttle controls should be reinstated
+    //i.e. the vehicle does not need to be restarted to reset an implausibility fault.
 }
+
 
 // loop code
 void loop() {
