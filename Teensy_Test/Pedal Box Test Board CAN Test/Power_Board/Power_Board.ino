b581fa7267b151b2a182387a4865410ceb037c7d
Nathan Cheek <nathan@nathancheek.com>
2016-10-25 19:17:21 -0400

    Add testing code for Power Board / Power Board Test Interface / Pedal Box Test Board

--- Teensy_Test/Pedal Box Test Board CAN Test/Power_Board/Power_Board.ino
+++ Teensy_Test/Pedal Box Test Board CAN Test/Power_Board/Power_Board.ino
@@ -11,8 +11,11 @@ FlexCAN CAN(500000);
 static CAN_message_t msg;
 long msTimer = 0;
 
-Metro ssrEngage = Metro(5000);
-boolean ssrEngaged = false;
+boolean brakeLight = false;
+
+Metro latchTimer = Metro(1000);
+boolean shutdownCircuitInitialized = false;
+boolean shutdownCircuitInitializing = false;
 
 void setup() {
   Serial.begin(115200); //initialize serial for PC communication
@@ -38,27 +41,36 @@ void loop() {
     for (unsigned int i = 0; i < msg.len; i += 2) {
       int value = msg.buf[i] + (msg.buf[i+1] << 8);
       float voltage = (float)value / 155.1515;
+      if (i == 0) {
+        if (voltage >= 2) {
+          brakeLight = true;
+        }
+        else {
+          brakeLight = false;
+        }
+      }
+      if (i == 6 && voltage >= 3 && !shutdownCircuitInitialized && !shutdownCircuitInitializing) {
+        shutdownCircuitInitializing = true;
+        latchTimer.reset();
+        digitalWrite(10, HIGH);
+        digitalWrite(11, HIGH);
+      }
       Serial.print(voltage);
       Serial.print("V ");
     }
     Serial.print(millis());
     Serial.println();
-  }
 
-  /*
-   * Periodically turn on and off solid state relays
-   */
-  if (ssrEngage.check()) {
-    if (ssrEngaged) {
-      ssrEngaged = false;
-      digitalWrite(10, LOW);
-      digitalWrite(11, LOW);
-      digitalWrite(13, LOW);
-    } else {
-      ssrEngaged = true;
-      digitalWrite(10, HIGH);
-      digitalWrite(11, HIGH);
+    if (brakeLight) {
       digitalWrite(13, HIGH);
+    } else {
+      digitalWrite(13, LOW);
+    }
+
+    if (shutdownCircuitInitializing && latchTimer.check()) {
+      shutdownCircuitInitializing = false;
+      shutdownCircuitInitialized = true;
+      digitalWrite(10, LOW);
     }
   }
 }
