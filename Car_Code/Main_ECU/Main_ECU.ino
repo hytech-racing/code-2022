323ce003c4eb5ad8d4d047899c87b36a43ea7207
vchen36 <vchen36@gatech.edu>
2016-10-27 20:29:11 -0400

    some thermistor stuff

--- Car_Code/Main_ECU/Main_ECU.ino
+++ Car_Code/Main_ECU/Main_ECU.ino
@@ -9,9 +9,14 @@ int shutdownCircuit = 0; // voltage after calculation
 bool OKHSfault = false; // fault status of OKHS, fault if true
 bool DISCHARGE_OKfault = false; // fault status of DISCHARGE_OK, fault if true
 bool startPressed = false; // true if start button is pressed
-int thermTemp = 0; // temperature of onboard thermistor (after calculation)
+float thermTemp = 0.0; // temperature of onboard thermistor (after calculation)
+int thermValue = 0; //raw value from thermistor
 bool startupDone = false; // true when reached drive state
 bool softwareFault = false; // true when software fault found
+const int THERMISTORNOMINAL = 10000;
+const int TEMPERATURENOMINAL = 25;
+const int BCONSTANT = 3900;
+const int SERIESRESISTOR = 10000;
 
 // Values to check if IMD, BMS high
 const int IMD_High = 50;
@@ -41,58 +46,78 @@ void setup() {
 
 // loop code
 void loop() {
-    //check CAN for a message for software shutdown
-    if (!startupDone) {
-        switch (curState) {
-            case GLVinit:
-                curState = waitIMDBMS; //going straight to waitIMD unti further notice
-                break;
-            case waitIMDBMS:
-                if (softwareFault) {
-                    curState = fatalFault;
-                } else {
-                    if (DISCHARGE_OK >= BMS_High) { // if BMS is high
-                        if (OKHS >= IMD_High) { // if IMD is also high
-                            curState = waitStartButton; // both BMD and IMD are high, wait for start button press
-                        }
-                    }
-                }
-                break;
-            case waitDriver:
-                if (checkFatalFault()) {
-                    curState = fatalFault;
-                } else {
-                    /*can message for start button press received*/
-                    curState = closeLatch;
-                }
-                break;
-            case AIRClose: // equivalent to VCCAIR in Google Doc state diagram
-                initialTime = millis();
-                unsigned long curTime = millis();
-                while(curTime <= initialTime + 500){
-                    if (checkFatalFault()) {
-                        curState = fatalFault;
-                        break;
-                    }
-                    curTime = millis();
-                }
-                if (!(curState == State.fatalFault)) {
-                    curState = drive;
-                }
-                break;
-            case fatalFault:
-            case drive:
-            //send can message to throttle control
-        }
-    } else {
-    }
+    readValues();
+    Serial.print("TEMPERATURE: ");
+    Serial.println(thermTemp);
+    Serial.print("OKHS: ");
+    Serial.println(OKHS);
+    Serial.print("BMS: ");
+    Serial.println(DISCHARGE_OK);
+    Serial.println(thermValue);
+    delay(200);
+    
+//    //check CAN for a message for software shutdown
+//    if (!startupDone) {
+//        switch (curState) {
+//            case GLVinit:
+//                curState = waitIMDBMS; //going straight to waitIMD unti further notice
+//                break;
+//            case waitIMDBMS:
+//                if (softwareFault) {
+//                    curState = fatalFault;
+//                } else {
+//                    if (DISCHARGE_OK >= BMS_High) { // if BMS is high
+//                        if (OKHS >= IMD_High) { // if IMD is also high
+//                            curState = waitStartButton; // both BMD and IMD are high, wait for start button press
+//                        }
+//                    }
+//                }
+//                break;
+//            case waitDriver:
+//                if (checkFatalFault()) {
+//                    curState = fatalFault;
+//                } else {
+//                    /*can message for start button press received*/
+//                    curState = closeLatch;
+//                }
+//                break;
+//            case AIRClose: // equivalent to VCCAIR in Google Doc state diagram
+//                initialTime = millis();
+//                unsigned long curTime = millis();
+//                while(curTime <= initialTime + 500){
+//                    if (checkFatalFault()) {
+//                        curState = fatalFault;
+//                        break;
+//                    }
+//                    curTime = millis();
+//                }
+//                if (!(curState == State.fatalFault)) {
+//                    curState = drive;
+//                }
+//                break;
+//            case fatalFault:
+//            case drive:
+//            //send can message to throttle control
+//        }
+//    } else {
+//    }
 }
 
 bool readValues() {
     DISCHARGE_OK = analogRead(BMS_OK_PIN)/ 67.7;
     OKHS = analogRead(OKHS_PIN) / 67.7;
-    thermTemp = analogRead(THERMISTOR_PIN);
+    thermValue = analogRead(THERMISTOR_PIN);
     //compute actual temperature with math
+    float resistance = ((float) 1023 / (float) thermValue) - 1;
+    resistance = SERIESRESISTOR / resistance;
+    Serial.println(resistance);
+    thermTemp = resistance / THERMISTORNOMINAL;
+    thermTemp = log(thermTemp);
+    thermTemp /= BCONSTANT;
+    thermTemp += 1.0 / (TEMPERATURENOMINAL + 273.15);
+    thermTemp = 1.0 / thermTemp;
+    thermTemp -= 273.15;
+    return true;
 }
 
 bool checkFatalFault() { // returns true if fatal fault found ()
