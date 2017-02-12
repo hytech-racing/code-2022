b36b39b6af420569a690c836a191633b16ad5a51
Karvin Dassanayake <karvin9@gmail.com>
2017-02-12 17:29:50 -0500

    Constructors

--- HyTech17_Library/DCU_status.cpp
+++ HyTech17_Library/DCU_status.cpp
@@ -8,3 +8,15 @@
 DCU_status::DCU_status() {
     message = {};
 }
+
+DCU_status::DCU_status(uint8_t buf[8]) {
+  load(buf);
+}
+
+DCU_status::DCU_status(uint8_t btn_press_id, uint8_t light_active_1, uint8_t light_active_2, uint8_t rtds_state) {
+    set_btn_press_id(btn_press_id);
+    set_light_active_1(light_active_1);
+    set_light_active_2(light_active_2);
+    set_rtds_state(rtds_state);
+}
+
