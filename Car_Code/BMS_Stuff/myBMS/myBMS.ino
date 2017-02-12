e89d54385f228f1ceb605c69b7f1f3ba7d2e9041
stevesdawg <shrivathsav.seshan@gmail.com>
2017-02-12 16:41:56 -0500

    lots of miscellaneous files removed from tracking.

--- Car_Code/BMS_Stuff/myBMS/myBMS.ino
+++ Car_Code/BMS_Stuff/myBMS/myBMS.ino
@@ -2,6 +2,7 @@
  * Shrivathsav Seshan
  * Oct 23 2016
  * The BMS
+ * Modified
  */
 
 #include <Arduino.h>
@@ -34,19 +35,19 @@ uint16_t cell_voltages[TOTAL_IC][12]; // contains 12 battery cell voltages
 uint16_t aux_voltagess[TOTAL_IC][6]; // contains auxillary pin voltages (not batteries)
 
 /*!<
-  The tx_cfg[][6] stores the LTC6804 configuration data that is going to be written 
+  The tx_cfg[][6] stores the LTC6804 configuration data that is going to be written
   to the LTC6804 ICs on the daisy chain. The LTC6804 configuration data that will be
   written should be stored in blocks of 6 bytes. The array should have the following format:
-  
+
  |  tx_cfg[0][0]| tx_cfg[0][1] |  tx_cfg[0][2]|  tx_cfg[0][3]|  tx_cfg[0][4]|  tx_cfg[0][5]| tx_cfg[1][0] |  tx_cfg[1][1]|  tx_cfg[1][2]|  .....    |
  |--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|-----------|
  |IC1 CFGR0     |IC1 CFGR1     |IC1 CFGR2     |IC1 CFGR3     |IC1 CFGR4     |IC1 CFGR5     |IC2 CFGR0     |IC2 CFGR1     | IC2 CFGR2    |  .....    |
- 
+
 */
 uint8_t tx_cfg[TOTAL_IC][6]; // data defining how data will be written to daisy chain ICs.
 
 /*!<
-  the rx_cfg[][8] array stores the data that is read back from a LTC6804-1 daisy chain. 
+  the rx_cfg[][8] array stores the data that is read back from a LTC6804-1 daisy chain.
   The configuration data for each IC  is stored in blocks of 8 bytes. Below is an table illustrating the array organization:
 
 |rx_config[0][0]|rx_config[0][1]|rx_config[0][2]|rx_config[0][3]|rx_config[0][4]|rx_config[0][5]|rx_config[0][6]  |rx_config[0][7] |rx_config[1][0]|rx_config[1][1]|  .....    |
@@ -188,7 +189,7 @@ void init_cfg()
         tx_cfg[i][0] = 0xFE;
         tx_cfg[i][1] = 0x00 ;
         tx_cfg[i][2] = 0x00 ;
-        tx_cfg[i][3] = 0x00 ; 
+        tx_cfg[i][3] = 0x00 ;
         tx_cfg[i][4] = 0x00 ;
         tx_cfg[i][5] = 0x00 ;
     }
