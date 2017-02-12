8eb9375b6991cabbfc38d48a7f9613afc0197822
Karvin Dassanayake <karvin9@gmail.com>
2017-02-12 16:58:30 -0500

    DCU: Initial functions for main file in library

---
+++ HyTech17_Library/DCU_status.cpp
@@ -0,0 +1,10 @@
+/*
+  DCU_status.cpp - HyTech Dashboard Control Unit CAN message: Status
+  Created by Karvin Dassanayake, February 12, 2017.
+ */
+
+#include "HyTech17.h"
+
+DCU_status::DCU_status() {
+    message = {};
+}
