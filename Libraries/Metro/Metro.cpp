f4711503805a37acccc4a9f26b487d830884a9f9
Karvin Dassanayake <karvin9@gmail.com>
2017-06-27 20:52:55 -0400

    Updated library folders for FlexCAN and Metro

--- Libraries/Metro/Metro.cpp
+++ Libraries/Metro/Metro.cpp
@@ -1,5 +1,4 @@
 
-
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
 #else
@@ -7,45 +6,58 @@
 #endif
 #include "Metro.h"
 
-Metro::Metro()
-{
-	
-	this->interval_millis = 1000;
-	
-}
-
 
 Metro::Metro(unsigned long interval_millis)
 {
-	
-	this->interval_millis = interval_millis;
-	
+	this->autoreset = 0;
+	interval(interval_millis);
+	reset();
 }
 
+// New creator so I can use either the original check behavior or benjamin.soelberg's
+// suggested one (see below). 
+// autoreset = 0 is benjamin.soelberg's check behavior
+// autoreset != 0 is the original behavior
+
+Metro::Metro(unsigned long interval_millis, uint8_t autoreset)
+{   
+	this->autoreset = autoreset; // Fix by Paul Bouchier
+	interval(interval_millis);
+	reset();
+}
 
 void Metro::interval(unsigned long interval_millis)
 {
   this->interval_millis = interval_millis;
 }
 
-uint8_t Metro::check()
-{
+// Benjamin.soelberg's check behavior:
+// When a check is true, add the interval to the internal counter.
+// This should guarantee a better overall stability.
 
-  unsigned long now = millis();
-  
-  if ( interval_millis == 0 ){
-    previous_millis = now;
-	return 1;
-  }
- 
-  if ( (now - previous_millis) >= interval_millis) {
-	#ifdef NOCATCH-UP
-	previous_millis = now ; 
-	#else
-	previous_millis += interval_millis ; 
-	 #endif
+// Original check behavior:
+// When a check is true, add the interval to the current millis() counter.
+// This method can add a certain offset over time.
+
+char Metro::check()
+{
+  if (millis() - this->previous_millis >= this->interval_millis) {
+    // As suggested by benjamin.soelberg@gmail.com, the following line 
+    // this->previous_millis = millis();
+    // was changed to
+    // this->previous_millis += this->interval_millis;
+    
+    // If the interval is set to 0 we revert to the original behavior
+    if (this->interval_millis <= 0 || this->autoreset ) {
+    	this->previous_millis = millis();
+	} else {
+		this->previous_millis += this->interval_millis; 
+	}
+    
     return 1;
   }
+  
+  
   
   return 0;
 
