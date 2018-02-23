4a333e2eddb54c01abf2f4ea1f8e833a17aa6be9
Brian Cai <briancai@gatech.edu>
2018-02-22 23:49:21 -0500

    this code works on the teensyduino

--- Car_Code/Display_Unit_4D_Systems/screen/screen.ino
+++ Car_Code/Display_Unit_4D_Systems/screen/screen.ino
@@ -8,19 +8,35 @@
 #define FALSE  (!TRUE)
 #endif
 
-int leddigits0; //battery temp high
-int leddigits1; //battery temp low
-int leddigits2; //Motor temp
-int leddigits3; // GLV Voltage
-int leddigits4; //Current Draw
-int gauge0; // Battery Level
-int coolgauge0; //Speed
+#define RESETLINE 2  // Change this if you are not using an Arduino Adaptor Shield Version 2 (see code below)
+
+Genie genie;
+int leddigits0 = 10; //battery temp high
+int leddigits1 = 11; //battery temp low
+int leddigits2 = 12; //Motor temp
+int leddigits3 = 13; // GLV Voltage
+int leddigits4 = 14; //Current Draw
+int gauge0 = 50; // Battery Level
+int coolgauge0 = 69; //Speed
 
 void setup() {
   // put your setup code here, to run once:
-  genieSetup(115200);
+  Serial1.begin(9600);
+  genie.Begin(Serial1);
 
 
+  //genie.AttachEventHandler(myGenieEventHandler);//Processes Events
+
+  //Reset display for that fresh in sync shit
+  //change RESETLINE to 2 for D2
+  pinMode(RESETLINE, OUTPUT);  // Set D4 on Arduino to Output (4D Arduino Adaptor V2 - Display Reset)
+  digitalWrite(RESETLINE, 0);  // Reset the Display via D4
+  delay(100);
+  digitalWrite(RESETLINE, 1);  // unReset the Display via D4
+
+  delay (3500); //let the display start up
+
+  genie.WriteContrast(15); 
 }
 
 void loop() {
@@ -34,11 +50,11 @@ void loop() {
 }
 
 void updateLCD() {
-  genieWriteObj (GENIE_OBJ_LED_DIGITS, 0, leddigits0);
-  genieWriteObj (GENIE_OBJ_LED_DIGITS, 1, leddigits1);
-  genieWriteObj (GENIE_OBJ_LED_DIGITS, 2, leddigits2);
-  genieWriteObj (GENIE_OBJ_LED_DIGITS, 3, leddigits3);
-  genieWriteObj (GENIE_OBJ_LED_DIGITS, 4, leddigits4);
-  genieWriteObj (GENIE_OBJ_GUAGE, 0, gauge0);
-  genieWriteObj (GENIE_OBJ_COOL_GAUGE, 0, coolgauge0);
+  genie.WriteObject(GENIE_OBJ_LED_DIGITS, 0, leddigits0);
+  genie.WriteObject(GENIE_OBJ_LED_DIGITS, 1, leddigits1);
+  genie.WriteObject(GENIE_OBJ_LED_DIGITS, 2, leddigits2);
+  genie.WriteObject(GENIE_OBJ_LED_DIGITS, 3, leddigits3);
+  genie.WriteObject(GENIE_OBJ_LED_DIGITS, 4, leddigits4);
+  genie.WriteObject(GENIE_OBJ_GAUGE, 0, gauge0);
+  genie.WriteObject(GENIE_OBJ_COOL_GAUGE, 0, coolgauge0);
 }
