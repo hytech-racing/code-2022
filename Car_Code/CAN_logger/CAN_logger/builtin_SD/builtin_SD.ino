1d5b90b19edf8ca532cf499e6e71d7edb35efd1c
Soohyun Kim <sokim@gatech.edu>
2018-11-06 20:42:47 -0500

    Completed Teensy 3.5 code for SD card logging

--- Car_Code/CAN_logger/CAN_logger/builtin_SD/builtin_SD.ino
+++ Car_Code/CAN_logger/CAN_logger/builtin_SD/builtin_SD.ino
@@ -1,6 +1,6 @@
 #include <SD.h>
 #include <SPI.h>
-#include <FlexCAN.h>
+#include <HyTech_FlexCAN.h>
 #include <HyTech_CAN.h>
 #include <kinetis_flexcan.h>
 #include <Wire.h>
@@ -13,9 +13,9 @@ static CAN_message_t msg;
 
 void setup()
 {
-  
- // Open serial communications and wait for port to open:
-  Serial.begin(9600); 
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
   CAN.begin();
   SD.begin(BUILTIN_SDCARD);
   logger = SD.open("test.txt", FILE_WRITE);
@@ -26,18 +26,23 @@ void setup()
 
 void loop()
 {
-	if (CAN.available()) {
+  if (CAN.available()) {
     CAN.read(msg);
     timestampWrite();
     logger.print(msg.id, HEX);
     logger.print(", ");
-    for (int i=0; i<8; i++) {
+    Serial.print(msg.id, HEX);
+    Serial.print(", ");
+    for (int i = 0; i < 8; i++) {
       logger.print(msg.buf[i], HEX);
       logger.print(" ");
+      Serial.print(msg.buf[i], HEX);
+      Serial.print(" ");
     }
     logger.println();
+    Serial.println();
     logger.flush();
-	}
+  }
 }
 
 void timestampWrite() {
@@ -49,7 +54,7 @@ void timestampWrite() {
   logger.print(" ");
   logger.print(month());
   logger.print(" ");
-  logger.print(year()); 
+  logger.print(year());
   logger.print("   ");
 }
 
@@ -58,10 +63,10 @@ time_t getTeensy3Time()
   return Teensy3Clock.get();
 }
 
-void writeDigits(int digits){
+void writeDigits(int digits) {
   // utility function for digital clock display: prints preceding colon and leading 0
   logger.print(":");
-  if(digits < 10)
+  if (digits < 10)
     logger.print('0');
   logger.print(digits);
 }
