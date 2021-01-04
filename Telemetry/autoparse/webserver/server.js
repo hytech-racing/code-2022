6dee0db4bc3d91eafcc4feeeb2d58c903a0b9843
Ubuntu <ubuntu@ip-172-31-21-176.us-east-2.compute.internal>
2021-01-04 00:30:11 +0000

    route modification

--- Telemetry/autoparse/webserver/server.js
+++ Telemetry/autoparse/webserver/server.js
@@ -13,9 +13,10 @@ app.use(bodyparser.urlencoded({ extended: true }));
 if (!fs.existsSync(UPLOAD_PATH))
 	fs.mkdirSync(UPLOAD_PATH)
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views/index.html')));
-app.get('/upload', (req, res) => res.sendFile(path.resolve(__dirname, 'views/upload.html')));
-app.get('/status', (req, res) => res.sendFile(path.resolve(__dirname, 'views/status.html')))
+app.get('/', (undefined, res) => res.redirect('/index.html'));
+app.get('/index.html', (req, res) => res.sendFile(path.resolve(__dirname, 'views/index.html')));
+app.get('/upload.html', (req, res) => res.sendFile(path.resolve(__dirname, 'views/upload.html')));
+app.get('/status.html', (req, res) => res.sendFile(path.resolve(__dirname, 'views/status.html')))
 
 app.post('/upload', bodyparser.json(), busboy({ highWaterMark: 2 * 1024 * 1024 }), (req, res) => {
 	var db_name, fstream, filename, filepath;
@@ -37,7 +38,7 @@ app.post('/upload', bodyparser.json(), busboy({ highWaterMark: 2 * 1024 * 1024 }
 
 	req.busboy.on('finish', () => {
         if (!db_name || !fstream) {
-			res.redirect('/upload?err=true');
+			res.redirect('/upload.html?err=true');
 			fs.unlinkSync(filepath);
 			return;
 		}
