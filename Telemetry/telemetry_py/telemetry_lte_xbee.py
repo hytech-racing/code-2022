02a03093148bc1273f91a03603569a3c88376a6d
ssmith40 <wiley.sharp40@gmail.com>
2021-10-13 23:07:17 -0400

    Update telemetry_lte_xbee.py

--- Telemetry/telemetry_py/telemetry_lte_xbee.py
+++ Telemetry/telemetry_py/telemetry_lte_xbee.py
@@ -8,8 +8,8 @@ sys.path.insert(1, '../telemetry_aws')
 from db import decode, unpack, fletcher16
 from console_config import CONSOLE_CONFIG
 
-MQTT_SERVER = 'localhost'
-# MQTT_SERVER = 'ec2-3-134-2-166.us-east-2.compute.amazonaws.com'
+#MQTT_SERVER = 'localhost'
+MQTT_SERVER = 'ec2-3-134-2-166.us-east-2.compute.amazonaws.com'
 # MQTT_SERVER = 'hytech-telemetry.ryangallaway.me'     # MQTT broker hostname
 
 MQTT_PORT   = 1883                    # MQTT broker port (non-SSL)
