d77618d4c7d9cf781f1d54d0035eac526c956c12
Liwei Sun <liweisun16@.gmail.com>
2021-11-08 17:56:04 -0500

    changed ecu and cooling current calculation equation match new current sensor, calibrated ecu sensor equation

--- Vehicle/Telemetry_Control_Unit/Telemetry_Control_Unit.ino
+++ Vehicle/Telemetry_Control_Unit/Telemetry_Control_Unit.ino
@@ -307,10 +307,8 @@ inline void read_analog_values() {
 
 void process_mcu_analog_readings() {
     //self derived
-    float current_ecu = (filtered_ecu_current_reading - 2048) / 151.0;
-    float current_cooling = (filtered_cooling_current_reading - 2048) / 151.0;
-    //Serial.println(current_cooling);
-    //Serial.println(current_ecu);
+    float current_ecu = (filtered_ecu_current_reading/819.2-2.467)/0.4;
+    float current_cooling = (filtered_cooling_current_reading - 2048) / 327.7;
 
     float glv_voltage_value = (((filtered_supply_reading/4096) * 5) * 55/12) + 0.14; //ADC->12V conversion + offset likely due to resistor values
     //Serial.print("GLV: ");
