da57694ab4edd0b426a29ebf1ac619171f96dda8
Yjaafar1 <70989403+Yjaafar1@users.noreply.github.com>
2022-01-27 20:08:48 -0500

    Completed Io Exapander -> 7-seg test

--- Playground/Dashboard_Testing/Dashboard_Testing.ino
+++ Playground/Dashboard_Testing/Dashboard_Testing.ino
@@ -18,18 +18,29 @@
 
 const uint8_t ADDR = 0; // address of the chip when  all address pins are grounded
 const uint8_t CS = 8;  // pin on Teensy that is used as Chip Select
+//See this link for LED segment information https://www.geeksforgeeks.org/seven-segment-displays/
+//These encodings correspond to the pins on the IO expander (0 = LSB to 7 = MSB)
+//Only pins 0 - 6 are needed (as the decimal on teh 7-segment is not used)
+//e.g. To encode 0 on the 7 segment, segments A-F must light up, while G is left off
+//The 7-seg LEDs share a common anode, meaning the pins on the IO expander must be set low
+//for the segment to light up. The encoding of 0 starts from the LSB, adding 6 zeros to illuminate A-F
+//a 1 is then added to keep G off and the 7th pin is left off. This created the encoding 0b01000000 which
+//is sent via SPY to the IO expander
+
+//REWRITE THE ABOVE
 const uint8_t number_pins[10] = 
-                     {0b00000010, 0b10011110, 0b00100100, 0b10011110,
-                      0b10011000, 0b01001000,  0b01000000, 0b00011110,
-                      0b00000001, 0b00011000};
+                     {0b01000000, 0b01111001, 0b00100100, 0b00110000,
+                      0b00011001, 0b00010010, 0b00000010, 0b01111000,
+                      0b10000000, 0b00011000};
 
 MCP23S08 expander(ADDR, CS);  // MCP23S17 object for the chip with the specified address and chip select pin
 
 void setup() {
-  Serial.begin(9600);
+  
   delay(1000);
+  //Begin SPI communication with IO Expander
   expander.begin(); 
-  // Assign all pins as outputs and write all high to start
+  // Assign all pins on expander as outputs and write all high to start
   for (int i = 0; i < 8; i++) {
     expander.pinMode(i, OUTPUT);
     expander.digitalWrite(i, HIGH);
@@ -37,6 +48,10 @@ void setup() {
 }
 
 void loop() {
-  expander.digitalWrite(0, LOW);
-  //expander.digitalWrite(0, value);     // output the reading from pin 7 on pin 0
+  //cycle through number encodings to display each on the 7-segment using the expander
+  for (int i = 0; i < 10; i++) {
+    expander.digitalWrite(number_pins[i]);
+    delay(1000);
+  }
+  
 }
