9d40ef804dda3a16e32c7a5c8d61cfdb684fdf88
Bo Han Zhu <bohanzhu9@gmail.com>
2022-03-17 22:15:21 -0400

    Increase SAB TCU timer and fix SAB parser int type

--- Telemetry/telemetry_parsers/parser.py
+++ Telemetry/telemetry_parsers/parser.py
@@ -819,8 +819,8 @@ def parse_ID_SAB_READINGS_FRONT(raw_message):
     message = "SAB_readings_front"
     labels = ["fl_susp_lin_pot", "fr_susp_lin_pot"]
     values = [
-        hex_to_decimal(raw_message[0:4], 16, True) / Multipliers.SAB_READINGS_NON_GPS.value,
-        hex_to_decimal(raw_message[4:8], 16, True) / Multipliers.SAB_READINGS_NON_GPS.value
+        hex_to_decimal(raw_message[0:4], 16, False) / Multipliers.SAB_READINGS_NON_GPS.value,
+        hex_to_decimal(raw_message[4:8], 16, False) / Multipliers.SAB_READINGS_NON_GPS.value
     ]
     units = ["mm", "mm"]
     return [message, labels, values, units]
@@ -829,10 +829,10 @@ def parse_ID_SAB_READINGS_REAR(raw_message):
     message = "SAB_readings_rear"
     labels = ["amb_air_hum", "amb_air_temp", "bl_susp_lin_pot", "br_susp_lin_pot"]
     values = [
-        hex_to_decimal(raw_message[0:4], 16, True) / Multipliers.SAB_READINGS_NON_GPS.value,
+        hex_to_decimal(raw_message[0:4], 16, False) / Multipliers.SAB_READINGS_NON_GPS.value,
         hex_to_decimal(raw_message[4:8], 16, True) / Multipliers.SAB_READINGS_NON_GPS.value,
-        hex_to_decimal(raw_message[8:12], 16, True) / Multipliers.SAB_READINGS_NON_GPS.value,
-        hex_to_decimal(raw_message[12:16], 16, True) / Multipliers.SAB_READINGS_NON_GPS.value
+        hex_to_decimal(raw_message[8:12], 16, False) / Multipliers.SAB_READINGS_NON_GPS.value,
+        hex_to_decimal(raw_message[12:16], 16, False) / Multipliers.SAB_READINGS_NON_GPS.value
     ]
     units = ["%", "C", "mm", "mm"]
     return [message, labels, values, units]
