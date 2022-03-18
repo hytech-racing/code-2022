caa4448c0a6cc1acc22c9c9a4dae91a8df056f05
Bo Han Zhu <bohanzhu9@gmail.com>
2022-03-18 17:22:25 -0400

    Merge SAB plotter into main plotter

--- Telemetry/telemetry_parsers/dataPlots.m
+++ Telemetry/telemetry_parsers/dataPlots.m
@@ -227,3 +227,43 @@ xlabel('Current')
 ylabel('Voltage')
 title('Accumulator Voltage Drop Analysis')
 
+%% IMU
+figure
+
+lat_accel = S.lat_accel;
+long_accel = S.long_accel;
+vert_accel = S.vert_accel;
+yaw = S.yaw;
+pitch = S.pitch;
+roll = S.roll;
+hold on
+plot(lat_accel(:,1),lat_accel(:,2));
+plot(long_accel(:,1),long_accel(:,2));
+plot(vert_accel(:,1),vert_accel(:,2));
+plot(yaw(:,1),yaw(:,2));
+plot(pitch(:,1),pitch(:,2));
+plot(roll(:,1),roll(:,2));
+legend({'Lateral Acceleration (m/s^2)','Longitudinal Acceleration (m/s^2)','Vertical Acceleration (m/s^2)','Yaw (deg/s)','Pitch (deg/s)', 'Roll (deg/s)'})
+xlabel('Time (ms)')
+title('IMU Accelerometer and Gyroscope')
+
+%% SAB
+figure
+
+cooling_loop_fluid_temp = S.cooling_loop_fluid_temp;
+amb_air_temp = S.amb_air_temp;
+fl_susp_lin_pot = S.fl_susp_lin_pot;
+fr_susp_lin_pot = S.fr_susp_lin_pot;
+bl_susp_lin_pot = S.bl_susp_lin_pot;
+br_susp_lin_pot = S.br_susp_lin_pot;
+hold on
+plot(cooling_loop_fluid_temp(:,1),cooling_loop_fluid_temp(:,2));
+plot(amb_air_temp(:,1),amb_air_temp(:,2));
+plot(fl_susp_lin_pot(:,1),fl_susp_lin_pot(:,2));
+plot(fr_susp_lin_pot(:,1),fr_susp_lin_pot(:,2));
+plot(bl_susp_lin_pot(:,1),bl_susp_lin_pot(:,2));
+plot(br_susp_lin_pot(:,1),br_susp_lin_pot(:,2));
+legend({'Cooling Loop Fluid Temperature (C)','Ambient Air Temperature (C)','Front-Left Suspension Linear Potentiometer (mm)','Front-Right Suspension Linear Potentiometer (mm)','Back-Left Suspension Linear Potentiometer (mm)', 'Back-Right Suspension Linear Potentiometer (mm)'})
+xlabel('Time (ms)')
+title('Sensor Acquisition Board All Sensors')
+
