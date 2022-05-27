2670db19ad49e15dc1cecab40f6940e0c8fd877f
liweisun <53801391+liweisun@users.noreply.github.com>
2022-05-26 20:16:26 -0400

    balance when charger is enabled

--- Vehicle/AMS_Control_Unit/AMS_Control_Unit.ino
+++ Vehicle/AMS_Control_Unit/AMS_Control_Unit.ino
@@ -100,6 +100,7 @@ BMS_temperatures bms_temperatures; //Message class containing general temperatur
 BMS_onboard_temperatures bms_onboard_temperatures; //Message class containing general AMS temperature information
 BMS_detailed_voltages bms_detailed_voltages; //Message class containing detailed voltage information
 BMS_detailed_temperatures bms_detailed_temperatures; // message class containing detailed temperature information
+CCU_status ccu_status;
 
 
 void setup() {
@@ -140,7 +141,7 @@ void loop() {
     print_gpios();
     print_timer.reset();
   }
-  if (bms_status.get_state() == BMS_STATE_CHARGING && BALANCE_ON && gpio_temps[max_board_temp_location[0]][max_board_temp_location[1]] <= 80) {
+  if (bms_status.get_state() == BMS_STATE_CHARGING && BALANCE_ON && gpio_temps[max_board_temp_location[0]][max_board_temp_location[1]] <= 80 && ccu_status.get_charger_enabled()) { 
     balance_cells();
   }
 }
@@ -397,6 +398,7 @@ void balance_cells() {
 void parse_CAN_CCU_status() {
   while (CAN.read(msg)) {
     if (msg.id == ID_CCU_STATUS) {
+      ccu_status.load(msg.buf);
       charging_timer.reset();
       if (bms_status.get_state() == BMS_STATE_DISCHARGING) {
         bms_status.set_state(BMS_STATE_CHARGING);
