422d0de9079612c33a4ddd381644762e88b7a488
liweisun <53801391+liweisun@users.noreply.github.com>
2022-06-10 18:24:02 -0400

    first commit

--- Vehicle/AMS_Control_Unit/AMS_Control_Unit.ino
+++ Vehicle/AMS_Control_Unit/AMS_Control_Unit.ino
@@ -102,6 +102,10 @@ BMS_detailed_voltages bms_detailed_voltages; //Message class containing detailed
 BMS_detailed_temperatures bms_detailed_temperatures; // message class containing detailed temperature information
 
 
+bool firstVoltageRead = true;
+uint16_t initial_cell_voltages[TOTAL_IC][12];
+uint16_t cell_voltages_deltas[TOTAL_IC][12];
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(6, OUTPUT);
@@ -133,6 +137,19 @@ void loop() {
     bms_status.set_state(BMS_STATE_DISCHARGING);
   }
   read_voltages();
+  if(firstVoltageRead){
+    for(int i=0; i<TOTAL_IC;i++){
+    for(int j = 0; j< 12;j++){
+      initial_cell_voltages[i][j] = cell_voltages[i][j];
+    }
+  }
+    firstVoltageRead = false;
+  }
+  for(int i=0; i<TOTAL_IC;i++){
+    for(int j = 0; j< 12;j++){
+      cell_voltages_deltas[i][j] = initial_cell_voltages[i][j] - cell_voltages[i][j];
+    }
+  }
   read_gpio();
   write_CAN_messages();
   if (print_timer.check()) {
@@ -552,6 +569,16 @@ void print_voltages() {
     Serial.print("\t\t");
     for (int cell = 0; cell < EVEN_IC_CELLS; cell++) {
       Serial.print(cell_balance_status[ic][cell]); Serial.print("\t");
+    }
+    Serial.println();
+  }
+  Serial.println();
+  Serial.println("DELTAS: INITIAL MINUS CURRENT VOLTAGE");
+  Serial.print("\tC0\tC1\tC2\tC3\tC4\tC5\tC6\tC7\tC8\tC9\tC10\tC11\t\t"); Serial.println("\tC0\tC1\tC2\tC3\tC4\tC5\tC6\tC7\tC8\tC9\tC10\tC11");
+  for (int ic = 0; ic < TOTAL_IC; ic++) {
+    Serial.print("IC"); Serial.print(ic); Serial.print("\t");
+    for (int cell = 0; cell < EVEN_IC_CELLS; cell++) {
+      Serial.print(cell_voltages_deltas[ic][cell] / 10000.0, 4); Serial.print("V\t");
     }
     Serial.println();
   }
