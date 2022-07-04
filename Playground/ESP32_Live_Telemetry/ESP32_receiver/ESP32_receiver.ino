4abf0c556e94a19a7915e723b22db38caffc5435
Bo Han Zhu <bohanzhu9@gmail.com>
2022-07-03 21:56:47 -0400

    Re-organize ESP32 Playground

--- Playground/ESP32_Live_Telemetry/ESP32_receiver/ESP32_receiver.ino
+++ Playground/ESP32_Live_Telemetry/ESP32_receiver/ESP32_receiver.ino
@@ -4,21 +4,6 @@
  * @author: Bo Han Zhu
  * @date: 03/31/2022
  */
- 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-// Global Variables and Imports
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// FreeRTOS
-#if CONFIG_FREERTOS_UNICORE
-  static const BaseType_t app_cpu = 0;
-#else
-  static const BaseType_t app_cpu = 1;
-#endif
-
-#define QUEUE_LEN 255
-#define led_pin LED_BUILTIN
-static QueueHandle_t msg_queue;
 
 // ESP-Now
 #include <esp_now.h>
@@ -26,106 +11,28 @@ static QueueHandle_t msg_queue;
 
 typedef struct transmit_msg_t {
   uint16_t  can_id;
-  uint8_t   raw_data[8];
+  uint64_t  raw_data;
 } transmit_msg_t;
 
-static transmit_msg_t incoming_msg;
+static transmit_msg_t msg;
 void data_receive(const uint8_t * mac, const uint8_t *incomingData, int len);
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-// Task Functions
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-/**
- * @brief: Receives messages transmitted via ESP-Now and adds to the raw message queue.
- */
-void add_incoming_msg(void * no_params) {
+void setup() {
+  Serial.begin(115200); // Must match baudrate of Python script that expects serial data
+  
   //ESP-Now connecion
   WiFi.mode(WIFI_STA); // Set ESP32 to stationary mode
 
   if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
+    printf("Error initializing ESP-NOW\n");
     exit(0); // Terminate program if cannot establish ESP-Now comms and wait for reset
   }
-
+  
   esp_now_register_recv_cb(data_receive); // Register callback function
 }
 
-/**
- * @brief: Spies on the message queue and sends to serial
- */
-void print_to_serial(void * no_params) {
-  transmit_msg_t msg;
-
-  while (true) {
-    // If message exists in the queue, pass to serial "can_id,raw_data"
-    if (xQueueReceive(msg_queue, (void *)&msg, 0) == pdTRUE) {
-      Serial.println(msg.can_id + "," + msg.raw_data[0] + msg.raw_data[1] + msg.raw_data[2] + msg.raw_data[3]
-                                      + msg.raw_data[4] + msg.raw_data[5] + msg.raw_data[6] + msg.raw_data[7]);
-    }
-  }
-}
-
-/**
- * @brief: Toggles LED if there is a message in the queue
- *         Waits 100 ms before checking again if LED is toggled
- */
-void blink_led(void * no_params) {
-  pinMode(led_pin, OUTPUT);
-
-  while (true) {
-    if (uxQueueMessagesWaiting(msg_queue) > 0)
-    {
-      digitalWrite(led_pin, HIGH);
-    }
-    else
-    {
-      digitalWrite(led_pin, LOW);
-    }
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-  }
-}
-
-/**
- * @brief: Entry point to FreeRTOS framework
- */
-void setup() {
-  //Initialize resources
-  Serial.begin(115200); // Must match baudrate of Python script that expects serial data
-  msg_queue = xQueueCreate(QUEUE_LEN, sizeof(transmit_msg_t));
-
-  // Create tasks
-  xTaskCreatePinnedToCore(
-    add_incoming_msg,
-    "Add Incoming Message",
-    2048,
-    NULL,
-    3,// Highest Priority Task
-    NULL,
-    app_cpu);
-
-  xTaskCreatePinnedToCore(
-    print_to_serial,
-    "Print to Serial",
-    1024,
-    NULL,
-    2, 
-    NULL,
-    app_cpu);
-
-  xTaskCreatePinnedToCore(
-    blink_led,
-    "Blink LED",
-    1024,
-    NULL,
-    1, // Lowest Priority Task
-    NULL,
-    app_cpu);
-  
-  vTaskDelete(NULL); // Delete setup() and loop() task
-}
-
 void loop() {
-  // Not needed in FreeRTOS Implementation
+  // Not needed
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -139,6 +46,6 @@ void loop() {
  * @param[in]: len - length of the data received, automatically filled in and not used
  */
 void data_receive(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&incoming_msg, incomingData, sizeof(transmit_msg_t));  // Copy in data
-  xQueueSend(msg_queue, (void *) &incoming_msg, portMAX_DELAY);
+  memcpy(&msg, incomingData, sizeof(msg));
+  printf("%u,%u", msg.can_id, msg.raw_data);
 }
--- Playground/ESP32_Live_Telemetry/ESP32_transmitter/ESP32_transmitter.ino
+++ Playground/ESP32_Live_Telemetry/ESP32_transmitter/ESP32_transmitter.ino
@@ -1,64 +1,9 @@
-/**
- * @brief: Functions as the rear SAB to handle four analog sensors and send all CAN msgs via ESP-Now to the receiver.
+/*
+ * @brief: ESP32 test code for the rear Sensor Acquisition Board.
+ *         Only provides existing rear SAB functionality, i.e. CAN comms and sensor reading.
  * 
  * @author: Bo Han Zhu
- * @date: 04/06/2022
- */
- 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-// Global Variables and Imports
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @brief: FreeRTOS
- */
-
-#if CONFIG_FREERTOS_UNICORE
-  static const BaseType_t app_cpu = 0;
-#else
-  static const BaseType_t app_cpu = 1;
-#endif
-
-#include "driver/gpio.h"
-
-#define QUEUE_LEN 255
-#define QUEUE_LED GPIO_NUM_11
-static QueueHandle_t msg_queue;
-static SemaphoreHandle_t serial_mutex; // The serial monitor is a shared resource, need a mutex to lock between task-sharing
-static SemaphoreHandle_t can_mutex; // SAB rear and CAN tasks both need to block CAN bus sharing
-
-/**
- * @brief: ESP-Now
- */
-
-#include <esp_now.h>
-#include <WiFi.h>
-
-// TODO: REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t broadcastAddress[] = {0x7C, 0x9E, 0xBD, 0x37, 0xCA, 0x84};
-
-typedef struct transmit_msg_t {
-  uint16_t  can_id;
-  uint8_t   raw_data[8];
-} transmit_msg_t;
-
-transmit_msg_t msg;
-
-void data_sent(const uint8_t *mac_addr, esp_now_send_status_t status);
-
-/**
- * @brief: CAN bus
- */
-
-// Reference: https://docs.espressif.com/projects/esp-idf/en/release-v3.3/api-reference/peripherals/can.html
-// Using built-in ESP32 CAN library
-#include "driver/can.h"
-#define CAN_TX GPIO_NUM_17
-#define CAN_RX GPIO_NUM_16
-can_message_t can_message_rx;
-
-/**
- * @brief: SAB rear
+ * @date: 04/16/2022
  */
 
 /*
@@ -68,10 +13,18 @@ can_message_t can_message_rx;
  * 2: Back-Left Suspension Linear Potentiometer       A3 (GPIO_NUM_39)
  * 3: Back-Right Suspension Linear Potentiometer      A2 (GPIO_NUM_34)
  */
+
 #include "SAB_readings_rear.h"
 #include "CAN_ID.h"
 #include "Metro.h"
+#include "driver/can.h"
+#include "driver/gpio.h"
+
+// CAN Variables
+#define CAN_TX GPIO_NUM_17
+#define CAN_RX GPIO_NUM_16
 Metro timer_rear_update = Metro(200);
+can_message_t can_message_rx;
 can_message_t can_message_tx;
 SAB_readings_rear sab_readings_rear;
 
@@ -86,29 +39,74 @@ SAB_readings_rear sab_readings_rear;
 // Functions scale value up by 1000, will get scaled down 1000 later in parser
 #define TICKS_4096_TO_1024 (4096 / 1024)         // ESP32 ADC translates to 4096 ticks per 3.3V instead of Teensy's 1024 ticks per 3.3V
 inline float get_sensor1_value() {return 0.0;}   // Needs to be implemented for cooling loop fluid temp
-inline float get_sensor2_value() {return (analogRead(SENSOR_2_CHANNEL) / TICKS_4096_TO_1024 * -0.43003 + 190.95588) * 1000;} // DO NOT CHANGE THIS W/O SPECIAL REASON
+inline float get_sensor2_value() {return 0.0;}   // Ambient Air Temperature sensor was lost :(
+// inline float get_sensor2_value() {return (analogRead(SENSOR_2_CHANNEL) * -0.43003 + 190.95588) * 1000;} // DO NOT CHANGE THIS W/O SPECIAL REASON
 inline float get_sensor3_value() {return (analogRead(SENSOR_3_CHANNEL) / TICKS_4096_TO_1024 * 0.059312 + 3.0) * 1000;}       // DO NOT CHANGE THIS W/O SPECIAL REASON
 inline float get_sensor4_value() {return (analogRead(SENSOR_4_CHANNEL) / TICKS_4096_TO_1024 * 0.059312 + 3.0) * 1000;}       // DO NOT CHANGE THIS W/O SPECIAL REASON
 
-// Variables to store filtered values
+// Options
+#define DEBUG (true)
+
+/*
+ * Variables to store filtered values
+ */
 float filtered_sensor1_reading{};
 float filtered_sensor2_reading{};
 float filtered_sensor3_reading{};
 float filtered_sensor4_reading{};
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-// Task Functions
-/////////////////////////////////////////////////////////////////////////////////////////////////////
 /**
- * @brief Takes messages from the queue and transmits them via ESP-Now
+ * @brief: ESP-Now
  */
-void transmit_message(void * no_params) {
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+// TODO: REPLACE WITH YOUR RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0x7C, 0x9E, 0xBD, 0x47, 0x5A, 0x14};
+
+
+typedef struct transmit_msg_t {
+  uint16_t  can_id;
+  uint64_t  raw_data;
+} transmit_msg_t;
+
+transmit_msg_t msg;
+
+void data_sent(const uint8_t *mac_addr, esp_now_send_status_t status);
+
+
+void setup() {
+  pinMode(CAN_LED, OUTPUT);
+
+  //Initialize configuration structures using macro initializers
+  can_general_config_t g_config = CAN_GENERAL_CONFIG_DEFAULT(CAN_TX, CAN_RX, CAN_MODE_NORMAL);
+  can_timing_config_t t_config = CAN_TIMING_CONFIG_500KBITS();
+  can_filter_config_t f_config = CAN_FILTER_CONFIG_ACCEPT_ALL();
+
+  //Install CAN driver
+  if (can_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
+      printf("Driver installed\n");
+  } else {
+      printf("Failed to install driver\n");
+      return;
+  }
+
+  //Start CAN driver
+  if (can_start() == ESP_OK) {
+    printf("Driver started\n");
+    digitalWrite(CAN_LED, HIGH);
+  } else {
+    printf("Failed to start driver\n");
+    digitalWrite(CAN_LED, LOW);
+    return;
+  }
+
+  // Start ESP-NOW
   WiFi.mode(WIFI_STA);
   
   if (esp_now_init() != ESP_OK) {
-    xSemaphoreTake(serial_mutex, portMAX_DELAY);
-    Serial.println("Error initializing ESP-NOW");
-    xSemaphoreGive(serial_mutex);
+    printf("Error initializing ESP-NOW\n");
     return;
   }
 
@@ -119,201 +117,67 @@ void transmit_message(void * no_params) {
   peerInfo.channel = 0;  
   peerInfo.encrypt = false;     
   if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    xSemaphoreTake(serial_mutex, portMAX_DELAY);
-    Serial.println("Failed to add peer");
-    xSemaphoreGive(serial_mutex);
+    printf("Failed to add peer\n");
     return;
   }
 
-  while (true) {
-    if (xQueueReceive(msg_queue, (void *)&msg, 0) == pdTRUE) {
-      // Determines outcome of transaction
-      esp_err_t outcome = esp_now_send(broadcastAddress, (uint8_t *) &msg, sizeof(msg));
-      
-      xSemaphoreTake(serial_mutex, portMAX_DELAY);
-      if (outcome == ESP_OK) Serial.println("Mesage sent successfully!");
-      else Serial.println("Error sending the message");
-      xSemaphoreGive(serial_mutex);
-    }
-  }
-}
-
-/**
- * @brief: Toggles LED if there is a message in the queue
- *         Waits 100 ms before checking again if LED is toggled
- */
-void blink_msg_queue_led(void * no_params) {
-  pinMode(QUEUE_LED, OUTPUT);
-
-  while (true) {
-    if (uxQueueMessagesWaiting(msg_queue) > 0)
-    {
-      digitalWrite(QUEUE_LED, HIGH);
-    }
-    else
-    {
-      digitalWrite(QUEUE_LED, LOW);
-    }
-    vTaskDelay(pdMS_TO_TICKS(100));
-  }
-}
-
-/**
- * @brief: Spies on the CAN bus and adds incoming messages to the message queue
- *         Transmits SAB rear readings to the CAN bus
- */
-void handle_can_messages(void * no_params) {
-  pinMode(CAN_LED, OUTPUT);
-
-  //Initialize configuration structures using macro initializers
-  can_general_config_t g_config = CAN_GENERAL_CONFIG_DEFAULT(CAN_TX, CAN_RX, CAN_MODE_NORMAL);
-  can_timing_config_t t_config = CAN_TIMING_CONFIG_500KBITS();
-  can_filter_config_t f_config = CAN_FILTER_CONFIG_ACCEPT_ALL();
-
-  //Install CAN driver
-  xSemaphoreTake(serial_mutex, portMAX_DELAY);
-  if (can_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
-      printf("Driver installed\n");
-  } else {
-      printf("Failed to install driver\n");
-      return;
-  }
-  xSemaphoreGive(serial_mutex);
-
-  //Start CAN driver
-  xSemaphoreTake(serial_mutex, portMAX_DELAY);
-  if (can_start() == ESP_OK) {
-    Serial.println("Driver started");
-    digitalWrite(CAN_LED, HIGH);
-  } else {
-    Serial.println("Failed to start driver");
-    digitalWrite(CAN_LED, LOW);
-    return;
-  }
-  xSemaphoreGive(serial_mutex);
-
-  while (true) {
-    // Transmit rear SAB readings
-    if (timer_rear_update.check()) {
-      xSemaphoreTake(can_mutex, portMAX_DELAY);
-      can_message_tx.identifier = ID_SAB_READINGS_REAR;
-      can_message_tx.flags = CAN_MSG_FLAG_NONE;
-      can_message_tx.data_length_code = sizeof(sab_readings_rear);
-      // Data has already been set in the rear sab functionality task
-      can_transmit(&can_message_tx, portMAX_DELAY);
-      xSemaphoreGive(can_mutex);
-    }
-
-    // Add CAN receive messages to queue
-    if (can_receive(&can_message_rx, portMAX_DELAY) == ESP_OK) {
-      // Print out incoming can message to console
-      xSemaphoreTake(serial_mutex, portMAX_DELAY);
-      Serial.print("ID is " + String(can_message_rx.identifier) + ", Data: ");
-      for (int i = 0; i < can_message_rx.data_length_code; i++) {
-        Serial.print(String(can_message_rx.data[i]));
-      }
-      Serial.println();
-      xSemaphoreGive(serial_mutex);
-
-      // Process received message
-      transmit_msg_t incoming_can_message;
-      incoming_can_message.can_id = can_message_rx.identifier;
-      for (int i = 0; i < 8; i++) {
-        incoming_can_message.raw_data[i] = (i < can_message_rx.data_length_code) ? can_message_rx.data[i] : 0u;
-      }
-
-      // Add to queue
-      xQueueSend(msg_queue, (void *) &incoming_can_message, portMAX_DELAY);
-    }
-  } // end while
-}
-
-void rear_sab_functionality(void * no_params) {
   //Get initial readings
   filtered_sensor1_reading = get_sensor1_value();
   filtered_sensor2_reading = get_sensor2_value();
   filtered_sensor3_reading = get_sensor3_value();
   filtered_sensor4_reading = get_sensor4_value();
+}
 
-  while (true) {
+void loop() { 
+
+  if (timer_rear_update.check()){
     // Set SAB readings rear
     sab_readings_rear.set_sensor_1(filtered_sensor1_reading);
     sab_readings_rear.set_sensor_2(filtered_sensor2_reading);
     sab_readings_rear.set_sensor_3(filtered_sensor3_reading);
     sab_readings_rear.set_sensor_4(filtered_sensor4_reading);
 
-    xSemaphoreTake(can_mutex, portMAX_DELAY);
     sab_readings_rear.write(can_message_tx.data);
-    xSemaphoreGive(can_mutex);
+    
+    can_message_tx.identifier = ID_SAB_READINGS_REAR;
+    can_message_tx.flags = CAN_MSG_FLAG_NONE;
+    can_message_tx.data_length_code = sizeof(sab_readings_rear);
+    can_transmit(&can_message_tx, portMAX_DELAY);
 
-    // Update sensor readings
-    filtered_sensor1_reading = ALPHA * filtered_sensor1_reading + (1 - ALPHA) * get_sensor1_value();
-    filtered_sensor2_reading = ALPHA * filtered_sensor2_reading + (1 - ALPHA) * get_sensor2_value();
-    filtered_sensor3_reading = ALPHA * filtered_sensor3_reading + (1 - ALPHA) * get_sensor3_value();
-    filtered_sensor4_reading = ALPHA * filtered_sensor4_reading + (1 - ALPHA) * get_sensor4_value();
+    #if DEBUG      
+    printf("-----------------------------\n");
+    printf("Sensor 1:\t");
+    printf("%f\n", filtered_sensor1_reading / 1000.0);
+    printf("Sensor 2:\t");
+    printf("%f\n", filtered_sensor2_reading / 1000.0);
+    printf("Sensor 3:\t");
+    printf("%f\n", filtered_sensor3_reading / 1000.0);
+    printf("Sensor 4:\t");
+    printf("%f\n\n", filtered_sensor4_reading / 1000.0);
+    #endif   
+  }
 
-    // Delay task 100 ms
-    vTaskDelay(pdMS_TO_TICKS(100));
+  // Software filtering
+  filtered_sensor1_reading = ALPHA * filtered_sensor1_reading + (1 - ALPHA) * get_sensor1_value();
+  filtered_sensor2_reading = ALPHA * filtered_sensor2_reading + (1 - ALPHA) * get_sensor2_value();
+  filtered_sensor3_reading = ALPHA * filtered_sensor3_reading + (1 - ALPHA) * get_sensor3_value();
+  filtered_sensor4_reading = ALPHA * filtered_sensor4_reading + (1 - ALPHA) * get_sensor4_value();
+
+  // Handle incoming CAN messages
+  if (can_receive(&can_message_rx, pdMS_TO_TICKS(20)) == ESP_OK) {
+    // Process received message
+    transmit_msg_t msg;
+    msg.can_id = can_message_rx.identifier;
+    memcpy(&msg.raw_data, can_message_rx.data, sizeof(uint64_t));
+
+    #if DEBUG
+    printf("Message ID: %u, Message Data %u\n", msg.can_id, msg.raw_data);
+    #endif
+
+    esp_err_t outcome = esp_now_send(broadcastAddress, (uint8_t *) &msg, sizeof(msg));
   }
 }
 
-/**
- * @brief: Entry point to FreeRTOS framework
- */
-void setup() {
-  // Initialize resources
-  Serial.begin(115200);
-  msg_queue = xQueueCreate(QUEUE_LEN, sizeof(transmit_msg_t));
-  serial_mutex = xSemaphoreCreateMutex();
-  can_mutex = xSemaphoreCreateMutex();
-
-  // Create tasks
-  xTaskCreatePinnedToCore(
-    transmit_message,
-    "Transmit Message",
-    2048,
-    NULL,
-    5,// Highest Priority Task
-    NULL,
-    app_cpu);
-  
-  xTaskCreatePinnedToCore(
-    handle_can_messages,
-    "Handle CAN Messages",
-    2048,
-    NULL,
-    4,
-    NULL,
-    app_cpu);
-  
-  xTaskCreatePinnedToCore(
-    rear_sab_functionality,
-    "Rear SAB Functionality",
-    2048,
-    NULL,
-    3,
-    NULL,
-    app_cpu);
-  
-  xTaskCreatePinnedToCore(
-    blink_msg_queue_led,
-    "Blink Message Queue LED",
-    1024,
-    NULL,
-    1, // Lowest Priority Task
-    NULL,
-    app_cpu);
-
-  vTaskDelete(NULL); // Delete setup() and loop() task
-}
-
-void loop() {
-  // Not needed in FreeRTOS Implementation
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-// Helper Functions
-/////////////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * @brief: Copies in the received raw message and adds it to the raw message queue
  * 
@@ -321,8 +185,7 @@ void loop() {
  * @param[in]: status - whether the ESP-Now message successfully transmitted or not
  */
 void data_sent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  xSemaphoreTake(serial_mutex, portMAX_DELAY);
-  Serial.print("\r\nStatus of Last Message Sent:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  xSemaphoreGive(serial_mutex);
+  printf("\r\nStatus of Last Message Sent:\t");
+  printf(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail\n");
+
 }
